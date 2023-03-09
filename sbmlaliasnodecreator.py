711eaded1deecdc59a4cb41514693abd5c7e242e
adelhpour <>
2023-03-09 14:09:00 -0800

    The function to get highly connected species is extracted

--- sbmlaliasnodecreator.py
+++ sbmlaliasnodecreator.py
@@ -10,18 +10,24 @@ class CreateAliasNode:
     def create(self, model, maximum_number_of_connected_species_reference_glyphs):
         self.extract_layout(model)
         if self.layout:
-            for species_glyph_index in range(self.layout.getNumSpeciesGlyphs()):
-                species_glyph = self.layout.getSpeciesGlyph(species_glyph_index)
-                number_of_connected_species_reference_glyphs = 0
-                for reaction_glyph_index in range(self.layout.getNumReactionGlyphs()):
-                    reaction_glyph = self.layout.getReactionGlyph(reaction_glyph_index)
-                    for species_reference_glyph_index in range(reaction_glyph.getNumSpeciesReferenceGlyphs()):
-                        species_reference_glyph = reaction_glyph.getSpeciesReferenceGlyph(
-                            species_reference_glyph_index)
-                        if species_reference_glyph.getSpeciesGlyphId() == species_glyph.getId():
-                            number_of_connected_species_reference_glyphs += 1
-                if number_of_connected_species_reference_glyphs > maximum_number_of_connected_species_reference_glyphs:
-                    print(species_glyph.getId())
+            highly_connected_species = self.get_highly_connected_species(maximum_number_of_connected_species_reference_glyphs)
+
+    def get_highly_connected_species(self, maximum_number_of_connected_species_reference_glyphs):
+        highly_connected_species = []
+        for species_glyph_index in range(self.layout.getNumSpeciesGlyphs()):
+            species_glyph = self.layout.getSpeciesGlyph(species_glyph_index)
+            number_of_connected_species_reference_glyphs = 0
+            for reaction_glyph_index in range(self.layout.getNumReactionGlyphs()):
+                reaction_glyph = self.layout.getReactionGlyph(reaction_glyph_index)
+                for species_reference_glyph_index in range(reaction_glyph.getNumSpeciesReferenceGlyphs()):
+                    species_reference_glyph = reaction_glyph.getSpeciesReferenceGlyph(
+                        species_reference_glyph_index)
+                    if species_reference_glyph.getSpeciesGlyphId() == species_glyph.getId():
+                        number_of_connected_species_reference_glyphs += 1
+            if number_of_connected_species_reference_glyphs > maximum_number_of_connected_species_reference_glyphs:
+                highly_connected_species.append({species_glyph.getId(), number_of_connected_species_reference_glyphs})
+
+        return highly_connected_species
 
     def extract_layout(self, model):
         self.check(model, "get model")
@@ -49,4 +55,4 @@ class CreateAliasNode:
 
 
 doc = sbml.readSBMLFromFile("/Users/home/Downloads/Model1.xml")
-CreateAliasNode(doc.getModel(), )
+CreateAliasNode(doc.getModel(), 1)
