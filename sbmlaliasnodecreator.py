e52f27f5ce76e6294c160fd7bd18366d2d0eebbd
adelhpour <>
2023-03-09 18:55:32 -0800

    Refactoring is applied

--- sbmlaliasnodecreator.py
+++ sbmlaliasnodecreator.py
@@ -10,20 +10,34 @@ class SBMLAliasNodeCreator:
         if maximum_number_of_connected_species_reference_glyphs > 0:
             self.extract_layout(model)
             if self.layout:
-                highly_connected_species = self.get_highly_connected_species(maximum_number_of_connected_species_reference_glyphs)
-                for species_info in highly_connected_species:
-                    original_species_glyph = self.layout.getSpeciesGlyph(species_info["id"])
-                    original_species_glyph.setId(species_info["id"] + "_0")
-                    number_of_required_alias_species_glyphs = len(species_info["connected_species_references"]) // maximum_number_of_connected_species_reference_glyphs
-                    for index_of_alias_species_glyphs in range(1, number_of_required_alias_species_glyphs):
-                        alias_species_glyph = self.layout.createSpeciesGlyph()
-                        alias_species_glyph.setId(species_info["id"] + "_" + str(index_of_alias_species_glyphs))
-                        alias_species_glyph.setSpeciesId(original_species_glyph.getSpeciesId())
-                        while len(species_info["connected_species_references"]) > (number_of_required_alias_species_glyphs - index_of_alias_species_glyphs) * maximum_number_of_connected_species_reference_glyphs:
-                            connected_species_reference = species_info["connected_species_references"].pop()
-                            connected_species_reference.setSpeciesGlyphId(alias_species_glyph.getId())
+                list_of_highly_connected_species = self.get_highly_connected_species_glyphs(maximum_number_of_connected_species_reference_glyphs)
+                for highly_connected_species in list_of_highly_connected_species:
+                    self.create_alias_species_glyphs(maximum_number_of_connected_species_reference_glyphs, highly_connected_species)
 
-    def get_highly_connected_species(self, maximum_number_of_connected_species_reference_glyphs):
+    def extract_layout(self, model):
+        self.check(model, "get model")
+        layout_plugin = model.getPlugin('layout')
+        self.check(layout_plugin, "get layout plugin")
+        number_of_layouts = layout_plugin.getNumLayouts()
+        if number_of_layouts:
+             self.layout = layout_plugin.getLayout(0)
+
+    @staticmethod
+    def check(value, message):
+        if value == None:
+            raise SystemExit('LibSBML returned a null value trying to ' + message + '.')
+        elif type(value) is int:
+            if value == libsbml.LIBSBML_OPERATION_SUCCESS:
+                return
+            else:
+                err_msg = 'Error encountered trying to ' + message + '.' \
+                          + 'LibSBML returned error code ' + str(value) + ': "' \
+                          + OperationReturnValue_toString(value).strip() + '"'
+                raise SystemExit(err_msg)
+        else:
+            return
+
+    def get_highly_connected_species_glyphs(self, maximum_number_of_connected_species_reference_glyphs):
         highly_connected_species = []
         for species_glyph_index in range(self.layout.getNumSpeciesGlyphs()):
             connected_species_references = []
@@ -43,31 +57,44 @@ class SBMLAliasNodeCreator:
 
         return highly_connected_species
 
-    def extract_layout(self, model):
-        self.check(model, "get model")
-        layout_plugin = model.getPlugin('layout')
-        self.check(layout_plugin, "get layout plugin")
-        number_of_layouts = layout_plugin.getNumLayouts()
-        if number_of_layouts:
-             self.layout = layout_plugin.getLayout(0)
+    def create_alias_species_glyphs(self, maximum_number_of_connected_species_reference_glyphs, species_info):
+        original_species_glyph = self.layout.getSpeciesGlyph(species_info["id"])
+        number_of_required_alias_species_glyphs = self.get_number_of_required_alias_species_glyphs(
+            maximum_number_of_connected_species_reference_glyphs, species_info["connected_species_references"])
+        for index_of_alias_species_glyphs in range(1, number_of_required_alias_species_glyphs + 1):
+            alias_species_glyph = self.layout.createSpeciesGlyph()
+            alias_species_glyph.setId(species_info["id"] + "_" + str(index_of_alias_species_glyphs))
+            self.set_alias_species_glyph_mutual_features(alias_species_glyph, original_species_glyph)
+            while len(species_info["connected_species_references"]) > (
+                    number_of_required_alias_species_glyphs - index_of_alias_species_glyphs + 1) * maximum_number_of_connected_species_reference_glyphs:
+                connected_species_reference = species_info["connected_species_references"].pop()
+                connected_species_reference.setSpeciesGlyphId(alias_species_glyph.getId())
+
+    @staticmethod
+    def get_number_of_required_alias_species_glyphs(maximum_number_of_connected_species_reference_glyphs,
+                                                    number_of_connected_species_references):
+        number_of_required_alias_species_glyphs = len(
+            number_of_connected_species_references) // maximum_number_of_connected_species_reference_glyphs
+        if len(number_of_connected_species_references) % maximum_number_of_connected_species_reference_glyphs == 0:
+            number_of_required_alias_species_glyphs -= 1
+        return number_of_required_alias_species_glyphs
+    def set_alias_species_glyph_mutual_features(self, alias_species_glyph, original_species_glyph):
+        alias_species_glyph.setSpeciesId(original_species_glyph.getSpeciesId())
+        self.set_alias_species_glyph_bounding_box(alias_species_glyph, original_species_glyph.getBoundingBox())
+
+    @staticmethod
+    def set_alias_species_glyph_bounding_box(alias_species_glyph, bounding_box):
+        alias_species_glyph.getBoundingBox().setX(bounding_box.getX())
+        alias_species_glyph.getBoundingBox().setY(bounding_box.getY())
+        alias_species_glyph.getBoundingBox().setWidth(bounding_box.getWidth())
+        alias_species_glyph.getBoundingBox().setHeight(bounding_box.getHeight())
 
 
-    def check(self, value, message):
-        if value == None:
-            raise SystemExit('LibSBML returned a null value trying to ' + message + '.')
-        elif type(value) is int:
-            if value == libsbml.LIBSBML_OPERATION_SUCCESS:
-                return
-            else:
-                err_msg = 'Error encountered trying to ' + message + '.' \
-                          + 'LibSBML returned error code ' + str(value) + ': "' \
-                          + OperationReturnValue_toString(value).strip() + '"'
-                raise SystemExit(err_msg)
-        else:
-            return
+
+
 
 
 
 doc = libsbml.readSBMLFromFile("/Users/home/Downloads/Model1.xml")
-SBMLAliasNodeCreator().create(doc.getModel(), 1)
+SBMLAliasNodeCreator().create(doc.getModel(), 5)
 libsbml.writeSBMLToFile(doc, "/Users/home/Downloads/Model2.xml")
