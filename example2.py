7f14b68aadd448018d0e090d1be2f48f85906098
Adel Heydarabadipour <36909449+adelhpour@users.noreply.github.com>
2023-03-22 14:08:21 -0700

    syntax update

--- example2.py
+++ example2.py
@@ -2,8 +2,9 @@ from sbmlaliasnodecreator import SBMLAliasNodeCreator
 
 input_sbml_file_name = "input_sbml_file_name.xml"
 output_sbml_file_name = "output_sbml_file_name.xml"
+maximum_number_of_connected_nodes = 4
 
 sbanc = SBMLAliasNodeCreator()
 sbanc.load(input_sbml_file_name)
-sbanc.create_alias(maximum_number_of_connected_nodes=4)
+sbanc.create_alias(maximum_number_of_connected_nodes=maximum_number_of_connected_nodes)
 sbanc.export(output_sbml_file_name)
