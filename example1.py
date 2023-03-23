657091c52dbe94251bcd901a8f80d79d7011c6da
adelhpour <>
2023-03-22 17:38:52 -0700

    cleaning the example

--- example1.py
+++ example1.py
@@ -2,7 +2,7 @@ from sbmlaliasnodecreator import SBMLAliasNodeCreator
 
 input_sbml_file_name = "input_sbml_file_name.xml"
 output_sbml_file_name = "output_sbml_file_name.xml"
-targeted_species_glyphs = [{'S1': 3}]
+targeted_species_glyphs = [{'S1': 2}, {'S2': 3}]
 
 sbanc = SBMLAliasNodeCreator()
 sbanc.load(input_sbml_file_name)
